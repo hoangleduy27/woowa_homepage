3d79e3d1bfa904c59208b288ba99db000dfdcb60
Nam Nguyen <you@example.com>
2023-07-31 15:28:10 +0700

    Add validate_register.js,validate_login.js then edit id, class of login, register page to suitable for js file

--- validate_login.js
+++ validate_login.js
@@ -2,70 +2,10 @@
 
 $(document).ready(function () {
 
-    $("#phone_email").on('keyup', function(){
-        var text = $("#phone_email").val();
-        const re = /^\S+@\S+\.\S+$/;
-        var number = /(((\+|)84)|0)(3|5|7|8|9)+([0-9]{8})\b/ ;
-
-        if (text.length == 0){
-            $("#error_phone_email").text("Xin vui lòng nhập số điện thoại hoặc email !");
-            $("#phone_email").css('border-style','solid').css('border-color','red');
-
-            return false;
-        } 
-
-        if (!text.match(re) && (!(text.match(number)))){
-            $("#error_phone_email").text("Số điện thoại hoặc email không hợp lệ !").css('color','red');
-            $("#phone_email").css('border-style','solid').css('border-color','red');
-
-        }
-
-        if (text.match(re) || ((text.match(number)))){
-            if (text.match(re)){
-                $("#error_phone_email").text("Email hoặc số điện thoại hợp lệ").css('color','green');
-                $("#phone_email").css('border-style','solid').css('border-color','green');
-
-            }
-            if ((text.match(number))){
-            //    if (text.length > 11){
-            //     $("#error_phone_email").text("Số điện thoại không được vượt quá 11 số !").css('color','red');
-            //     $("#phone_email").css('border-style','solid').css('border-color','red');
-
-            //    }
-            //    else
-               {
-                $("#error_phone_email").text("Email hoặc số điện thoại hợp lệ").css('color','green');
-                $("#phone_email").css('border-style','solid').css('border-color','green');
-
-            }
-            }
-        }
-    });
-
-
-    $("#pass_login").on('keyup', function(){
-        var password = $("#pass_login").val();
-        if (password.length == 0){
-            $("#error_password").text("Xin vui lòng nhập mật khẩu !").css('color', 'red');
-            $("#pass_login").css("border-style", 'solid').css("border-color", 'red');
-        }
-
-        if ((password.length > 0) || (password.length < 8)) {
-            $("#error_password").text("Mật khẩu phải có tối thiểu 8 kí tự !").css('color', 'red');
-            $("#pass_login").css("border-style", 'solid').css("border-color", 'red');
-        }
-        if (password.length > 8){
-            $("#error_password").text("");
-            $("#pass_login").css("border-style", 'solid').css("border-color", 'green');
-        }
-    })
-
+    //Click login button , check if input is null or not
     $("#login_btn").click(function(){
         text = $("#phone_email").val();
-        console.log(text);
         password = $("#pass_login").val();
-        const re = /^\S+@\S+\.\S+$/;
-        var number = /(((\+|)84)|0)(3|5|7|8|9)+([0-9]{8})\b/ ;
 
         if ((text.length == 0) && (password.length == 0)){
             $("small#error_phone_email").text("Xin vui lòng nhập mật khẩu !").css('color', 'red');
@@ -75,20 +15,15 @@ $(document).ready(function () {
 
         if ((text.length > 0)){
             $("#error_phone_email").text("");
-            $("#phone_email").css("border-style","solid").css("border-color", "green");
+            $("#phone_email").css("border-style", "none");
 
         }
 
-        if ((!text.match(re)) && (!(text.match(number)))){
-            $("#error_phone_email").text("Số điện thoại hoặc email không hợp lệ !").css('color','red');
-            $("#phone_email").css('border-style','solid').css('border-color','red');
-
+        if (password.length > 0){
+            $("#error_password").text("");
+            $("#pass_login").css("border-style", "none");
         }
-        if (text.match(re) || ((text.match(number)))){
-            $("#error_phone_email").text("");
-            $("#phone_email").css('border-style','solid').css('border-color','green');
 
-        }
         
 
         if (text.length == 0){
@@ -102,19 +37,7 @@ $(document).ready(function () {
 
         }
         
-        if ( (password.length > 0) &&  (password.length < 8)){
-            $("#error_password").text("Mật khẩu phải có tối thiểu 8 kí tự !");
-            $("#pass_login").css("border-style", "solid").css("border-color","red");
-
-        }
-
         
-        // if (!text.match(re) && (!isVietnamesePhoneNumber(text))){
-        //     $("#lb_email_phone").text("Email hoặc số điện thoại không hợp lệ !").css('color','red');
-        // }
-        // if (text.match(re) || (isVietnamesePhoneNumber(text))){
-        //     $("#lb_email_phone").text("Email hoặc số điện thoại hợp lệ").css('color','green');
-        // }
             
             
     });
