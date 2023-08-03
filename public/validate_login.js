42aefe6a76213364f676c7aa8c1505936145fd8a
Nam Nguyen <you@example.com>
2023-08-03 11:32:25 +0700

    Change login validation

--- public/validate_login.js
+++ public/validate_login.js
@@ -2,44 +2,33 @@
 
 $(document).ready(function () {
 
-    //Click login button , check if input is null or not
-    $("#login_btn").on('keyup',function(){
-        text = $("#phonenumberOrEmail").val();
-        password = $("#password").val();
 
-        if ((text.length == 0) && (password.length == 0)){
-            $("small#error_phone_email").text("Xin vui lòng nhập mật khẩu !").css('color', 'red');
-            $("small#error_password").text("Xin vui lòng nhập mật khẩu !").css('color', 'red');
-            $(".form-control").css("border-style", 'solid').css("border-color", 'red');
+    $('#phonenumberOrEmail').on("keyup", function(){
+        text = $("#phonenumberOrEmail").val();
+
+        if (text.length == 0){
+            $("#error_phone_email").text("Email hoặc số điện thoại không được để trống !").css('color','red');
+            $("#phonenumberOrEmail").css("border-style", "solid").css("border-color", "red");
         }
 
-        if ((text.length > 0)){
+        if (text.length > 0){
             $("#error_phone_email").text("");
             $("#phonenumberOrEmail").css("border-style", "none");
-
         }
+    });
 
+    $("#password").on("keyup", function(){
+        password = $("#password").val();
+        
+        if (password.length == 0){
+            $("#error_password").text("Mật khẩu không được để trống !").css('color', 'red');
+            $("#password").css("border-style", 'solid').css("border-color", "red");
+        }
+        
         if (password.length > 0){
             $("#error_password").text("");
             $("#password").css("border-style", "none");
         }
-
-        
-
-        if (text.length == 0){
-            $("#error_phone_email").text("Email hoặc số điện thoại không được để trống !").css('color','red');
-            $("#phonenumberOrEmail").css("border-style","solid").css("border-color", "red");
-        }
-        
-        if (password.length == 0){
-            $("#error_password").text("Mật khẩu không được để trống !").css('color', 'red');
-            $("#password").css("border-style","solid").css("border-color", "red");
-
-        }
-        
-        
-            
-            
     });
 });
 
